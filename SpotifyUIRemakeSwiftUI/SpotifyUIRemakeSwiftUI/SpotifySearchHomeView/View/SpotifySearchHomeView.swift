deb0ad48dc8f88588cc6463e728ba285ede4db08
Delstun McCray <87548497+Delstroo@users.noreply.github.com>
2023-12-16 09:59:54 -0800

    beginning the spotify search home view and viewmodel

--- SpotifyUIRemakeSwiftUI/SpotifyUIRemakeSwiftUI/SpotifySearchHomeView/View/SpotifySearchHomeView.swift
+++ SpotifyUIRemakeSwiftUI/SpotifyUIRemakeSwiftUI/SpotifySearchHomeView/View/SpotifySearchHomeView.swift
@@ -8,11 +8,61 @@
 import SwiftUI
 
 struct SpotifySearchHomeView: View {
+    @State private var songName = ""
+    var searchBarHeight = UIScreen.main.bounds.height / 21.04
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView(.vertical) {
+                HStack {
+                    // MARK: Header View
+                    RoundedRectangle(cornerRadius: .infinity)
+                        .frame(width: 35, height: 35)
+                        .foregroundColor(Color(hex: 0xBA9ACB))
+                        .overlay {
+                            Text("K")
+                                .bold()
+                                .foregroundStyle(.black)
+                        }
+                    
+                    Text("Search")
+                        .font(.title.bold())
+                    
+                    Spacer()
+                    
+                    Image(systemName: "camera")
+                        .resizable()
+                        .frame(width: 25, height: 22)
+                }
+                .padding(.horizontal, 10)
+                
+                // MARK: Text field
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12)
+                        .frame(height: searchBarHeight)
+                        .foregroundColor(.white)
+                        .overlay {
+                            
+                            HStack {
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundStyle(.black)
+                                    .padding(.leading)
+                                Text("What do you want to listen to?")
+                                    .foregroundColor(.black)
+                                    .preferredColorScheme(.light)
+                                    .font(.system(size: 16))
+                            
+                                Spacer()
+                            }
+                        }
+                }
+                .padding(.horizontal, 10)
+            }
+        }
     }
 }
 
 #Preview {
     SpotifySearchHomeView()
+        .preferredColorScheme(.dark)
 }
