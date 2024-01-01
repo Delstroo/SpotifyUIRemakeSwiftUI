975c5f22101c04da4a69d33d9593d5bc0330357d
Delstroo <delstunmccray@gmail.com>
2023-12-31 21:03:11 -0800

    adding the cell

---
+++ SpotifyUIRemakeSwiftUI/SpotifyUIRemakeSwiftUI/CurrentlyPlayingCell.swift
@@ -0,0 +1,20 @@
+//
+//  CurrentlyPlayingCell.swift
+//  SpotifyUIRemakeSwiftUI
+//
+//  Created by Delstun McCray on 12/31/23.
+//
+
+import SwiftUI
+
+struct CurrentlyPlayingCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CurrentlyPlayingCell_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentlyPlayingCell()
+    }
+}
