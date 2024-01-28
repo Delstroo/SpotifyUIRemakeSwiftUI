d7f21be0d21cc0b014e27663594cfa59318e63bd
Delstun McCray <87548497+Delstroo@users.noreply.github.com>
2024-01-27 18:12:41 -0800

    adding the base for saved view

---
+++ SpotifyUIRemakeSwiftUI/SpotifyUIRemakeSwiftUI/SpotifySavedHomeView/View/SpotifySavedHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SpotifySavedHomeView.swift
+//  SpotifyUIRemakeSwiftUI
+//
+//  Created by Delstun McCray on 1/27/24.
+//
+
+import SwiftUI
+
+struct SpotifySavedHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotifySavedHomeView()
+}
